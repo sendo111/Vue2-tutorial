6b26950961bd38934d1b77860bf6082ca0da3e91
s.endo <abcy.ns.gbd.9st@gmail.com>
2021-11-11 21:58:07 +0900

    :tada: udemy lern-vuejs section20

---
+++ lern-vuejs/section20/js/main.js
@@ -0,0 +1,24 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		colors: [
+			{ name: 'Red' },
+			{ name: 'Green' },
+			{ name: 'Blue' },
+		]
+	},
+	watch: {
+		colors: {
+			handler: function(newValue, oldValue) {
+				console.log('Update!');
+				console.log(
+					'new: %screen, oldValue',
+					JSON.stringify(newValue, null, '\t'),
+					JSON.stringify(oldValue, null, '\t')
+				);
+			},
+			deep: true,
+			immediate: true
+		}
+	}
+ })
